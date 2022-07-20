5196d6252c95cda03273b7161dff1bacf4177eda
mihaimaier <maiermihai90@gmail.com>
2022-07-21 01:29:39 +0300

    Add project files.

---
+++ Laborator11 - Exercitiu/Classes/ProduseAlimentare.cs
@@ -0,0 +1,28 @@
+﻿using Laborator11___Exercitiu.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laborator11___Exercitiu.Classes
+{
+    class ProduseAlimentare : Product, ITaxesCalculatorStrategy
+    {
+        private const string Currency = "RON";
+        public double ApplyTaxes()
+        {
+            double tax = 0.09;
+            double PriceAfterTax = GrossPrice + (GrossPrice * tax);
+            return this.PriceAfterTax;
+        }
+        public ProduseAlimentare(string Name, double GrossPrice) :base(Name,GrossPrice)
+        {
+            this.Name = Name;
+            this.GrossPrice = GrossPrice;
+        }
+        public string Name { get; }
+
+        public double GrossPrice { get; }
+
+        public double PriceAfterTax { get { return PriceAfterTax; } }
+    }
+}
---
+++ Laborator11 - Exercitiu/Classes/ProduseMenajere.cs
@@ -0,0 +1,31 @@
+﻿using Laborator11___Exercitiu.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laborator11___Exercitiu.Classes
+{
+    class ProduseMenajere : Product, ITaxesCalculatorStrategy
+    {
+        private const string Currency = "RON";
+        public double ApplyTaxes()
+        {
+            double tax = 0.19;
+            double PriceAfterTax = GrossPrice * tax;
+            return this.PriceAfterTax;
+        }
+        public ProduseMenajere(string Name, double GrossPrice) :base(Name,GrossPrice)
+        {
+            this.Name = Name;
+            this.GrossPrice = GrossPrice;
+        }
+        public string Name { get; }
+
+        public double GrossPrice { get; }
+
+        public double PriceAfterTax
+        {
+            get { return PriceAfterTax; }
+        }
+    }
+}
